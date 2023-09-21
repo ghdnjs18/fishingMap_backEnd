51549f006772c91949192d5b62a29aae6a9b467c
ghdnjs18 <ghdnjs18@naver.com>
2023-09-21 20:07:15 +0900

    CI/CD 시도 1.2

--- src/test/java/com/sparta/fishingload_backend/FishingLoadBackEndApplicationTests.java
+++ src/test/java/com/sparta/fishingload_backend/FishingLoadBackEndApplicationTests.java
@@ -1,13 +1,9 @@
 package com.sparta.fishingload_backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FishingLoadBackEndApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
